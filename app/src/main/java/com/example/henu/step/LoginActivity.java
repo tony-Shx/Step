a8d19be094ddbf233785a11c0040957d59aa55bf
tony_Shx <17839223557@163.com>
2017-03-26 23:09:35 +0800

    1.加入从登陆页面进入主页面后数据同步操作 2.加入登录时提示对话框，界面更友好

--- app/src/main/java/com/example/henu/step/LoginActivity.java
+++ app/src/main/java/com/example/henu/step/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.example.henu.step;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -13,9 +14,14 @@ import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.example.henu.step.Bean.Run;
+import com.example.henu.step.Bean.RunningRecord;
 import com.example.henu.step.Bean.User;
+import com.example.henu.step.DataBase.DatebaseAdapter;
+import com.example.henu.step.Util.DateHelper;
 
 import java.util.List;
 
@@ -28,6 +34,7 @@ public class LoginActivity extends Activity implements View.OnClickListener {
 
 	private Button button_register, button_login;
 	private EditText editText_telephone_login, editText_password_login;
+	private Dialog dialog;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -80,6 +87,13 @@ public class LoginActivity extends Activity implements View.OnClickListener {
 							.create();
 					dialog.show();
 				} else {
+					final AlertDialog.Builder normalDia = new AlertDialog.Builder(LoginActivity.this);
+					normalDia.setIcon(R.drawable.smssdk_dialog_btn_nor);
+					normalDia.setTitle("温馨提示：");
+					normalDia.setMessage("正在加载，请稍后");
+					normalDia.setView(new ProgressBar(this));
+					normalDia.setCancelable(false);
+					dialog = normalDia.show();
 					BmobQuery<User> query = new BmobQuery<User>();
 					//查询username叫“XX”的数据
 					query.addWhereEqualTo("telephone", telephone);
@@ -114,16 +128,15 @@ public class LoginActivity extends Activity implements View.OnClickListener {
 									normalDia.create().show();
 								} else {
 									if (password.equals(object.get(0).getPassword())){
-										Toast.makeText(getApplicationContext(), "登录成功", Toast.LENGTH_LONG).show();
+
 										SharedPreferences sp = getSharedPreferences("login",Context.MODE_PRIVATE);
 										SharedPreferences.Editor editor = sp.edit();
 										editor.putBoolean("isLogin",true);
 										editor.putString("telephone",telephone);
 										editor.putString("password",password);
 										editor.commit();
-										Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-										startActivity(intent);
-									    finish();
+										downloadRunningDate(telephone);
+
 									}else{
 										Toast.makeText(getApplicationContext(), "密码错误，请检查您的密码", Toast.LENGTH_LONG).show();
 									}
@@ -143,6 +156,64 @@ public class LoginActivity extends Activity implements View.OnClickListener {
 				break;
 		}
 
+	}
+
+	private void downloadRunningDate(final String telephone) {
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				BmobQuery<RunningRecord> query = new BmobQuery<RunningRecord>();
+				query.addWhereEqualTo("telephone", telephone);
+				query.setLimit(50);
+				query.findObjects(new FindListener<RunningRecord>() {
+					@Override
+					public void done(List<RunningRecord> list, BmobException e) {
+						if (e == null) {
+							for (RunningRecord runningRecord : list) {
+								String id = runningRecord.getObjectId();
+								DatebaseAdapter db = new DatebaseAdapter(getApplicationContext());
+								Log.i("BMOB_SplashActivity:", "e==null"+id);
+								Run run = db.findById(id);
+								if (run == null) {
+									run = new Run();
+									run.setId(id);
+									run.setTelephone(runningRecord.getTelephone());
+									long startTime = DateHelper.changeStringToDate(runningRecord.getStart_time());
+									long endTime = DateHelper.changeStringToDate(runningRecord.getEnd_time());
+									if (startTime > 0L && endTime > 0L) {
+										run.setStart_time(startTime / 1000 - 1483200000L);
+										run.setEnd_time(endTime / 1000 - 1483200000L);
+									} else {
+										run.setStart_time(0L);
+										run.setEnd_time(0L);
+									}
+									run.setLength(runningRecord.getLength());
+									run.setDuration(runningRecord.getDuration());
+									run.setConsume(runningRecord.getConsume());
+									run.setPoints(runningRecord.getPoints());
+									run.setUpdate(1);
+									db.add(run);
+								} else {
+									run.setUpdate(1);
+									db.update(run, run.getId());
+								}
+							}
+						} else {
+							Log.e("BMOB_SplashActivity:", "初始化数据同步出错（网络原因）" + e.getMessage());
+						}
+					}
+				});
+				try {
+					Thread.sleep(1500);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+				dialog.dismiss();
+				startActivity(intent);
+				finish();
+			}
+		}).start();
 	}
 
 }
--- app/src/main/java/com/example/henu/step/SplashActivity.java
+++ app/src/main/java/com/example/henu/step/SplashActivity.java
@@ -97,7 +97,6 @@ public class SplashActivity extends Activity {
 					}
 				});
 
-
 			}
 		}).start();
 	}
